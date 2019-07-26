2f80410d454c24a255aa2161155be3f0b610ede4
Luca Tacconi <luca.tacconi@monolite.net>
2019-07-26 09:22:51 +0200

    Dynamc redirect from root directory to /public/index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-header("Location: http://localhost/monolite/silly-vue-scaffolding/public/index.php");
+header("Location: ". (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") ."://". $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "public/index.php");
 
 exit;
